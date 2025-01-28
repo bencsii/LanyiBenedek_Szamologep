645fb894d838085904f594766f951bf6c76b7781
bencsii <benicke001@gmail.com>
2025-01-28 18:13:34 +0100

    indit_kesz

--- Szamologep_beni/Program.cs
+++ Szamologep_beni/Program.cs
@@ -38,8 +38,61 @@ namespace Szamologep_beni
 
 
 
+        static void indit(double num1, double num2)
+        {
+
+
+          
+            
+
+
+            Console.WriteLine("Osszeadas: +");
+            Console.WriteLine("Kivonas: -");
+            Console.WriteLine("Szorzas: *");
+            Console.WriteLine("Osztas: /");
+            Console.WriteLine("Szazalek: %"); 
+
+
+
+            Console.Write("Adja meg az elvegezendo muvelet jelet: ");
+            string muvelet = Console.ReadLine();
+
+            switch (muvelet)
+            {
+                case "+":
+                    Console.WriteLine($"A ket szam osszege: {osszeadas(num1, num2)}"); break;
+
+                case "-":
+                    Console.WriteLine($"A ket szam kulonbsege: {kivonas(num1, num2)}"); break;
+
+
+                case "*":
+                    Console.WriteLine($"A ket szam szorzata: {szorzas(num1, num2)}"); break;
+
+
+                case "/":
+                    Console.WriteLine($"Osztas v hogy hivjan nem ertek a matekhoz: {osztas(num1, num2)}"); break;
+
+
+                case "%":
+                    Console.WriteLine($"Szazalek szamitas: {szazalek(num1, num2)}"); break;
+
+
+            }
+
+
+        }
+
+
+
         static void Main(string[] args)
         {
+            Console.Write("Adja meg az elso szamot: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Adja meg a masodik szamot: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            indit(num1,num2);
         }
     }
 }
